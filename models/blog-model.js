37dfededd2ac5afc733dc5c2432b00d176a28837
honeybee <shelby.e.pagan@gmail.com>
2023-11-26 15:42:21 -0500

    add schema to blog-model

---
+++ models/blog-model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    author: { type: String, required: true },
+    content: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+const Blog = mongoose.model('Blog', blogSchemaSchema);
+module.exports = Blog;
