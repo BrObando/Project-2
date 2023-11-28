75c9081695f7a45cc72c00bbdf068a965f5e6c01
BrObando <brianna.obando@gmail.com>
2023-11-28 11:43:15 -0500

    Show profile coming up with status

--- controllers/profiles-controller.js
+++ controllers/profiles-controller.js
@@ -3,6 +3,7 @@ const Profile = require('../models/profile');
 
 module.exports = {
     updateProfile,
+    showProfile, 
   };
 
 
@@ -13,16 +14,32 @@ async function updateProfile(req, res) {
         const profileId = req.params.id;
         const {status} = req.body; 
       
-  await Profile.findByIdAndUpdate(profileId, status, {new: true} )
+  await Profile.findByIdAndUpdate(profileId, status, {new: true} ) // new tru - update // status - takes the info to update 
       
-  // res.render('v', { title: "Game Details", game });
-//  await profile.save();
+  res.render('profiles/update', { profile: updatedProfile });
   
       
     } catch (error) {
       console.error(err);
       
     }
+}
+
+async function showProfile(req, res) {
+  try {
+       const userId = req.user._id;
+        const profileId = req.params.id;
+        const {status} = req.body; 
+
+    // if (!req.isAuthenticated()) {
+    //   return res.redirect('/');
+    // }
+    const profile = await Profile.findById(profileId);
+
+    res.render('profiles/show', { profile, showProfile });
+  } catch (error) {
+    console.error(err);
+  }
 }
 
 // async function editProfile(req.res){
--- routes/profiles-router.js
+++ routes/profiles-router.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const profileCtrl = require('../controllers/profiles-controller');
 
-router.post('/profile/:id/update', profileCtrl.updateProfile)
-router.get('/profile/:id/edit', profileCtrl.editProfile)
+const ensureLoggedIn = require('../config/ensureLoggedIn')
+
+router.post('/:id/update', ensureLoggedIn, profileCtrl.updateProfile);
+router.get('/:id/show', ensureLoggedIn, profileCtrl.showProfile);
+// router.get('/profile/:id/edit', profileCtrl.editProfile)
 module.exports = router;
