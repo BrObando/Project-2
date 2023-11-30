3ab533f202e3fc41268c5f8ab2d940d9322bab1f
doniellekinchen <donicodes.things@gmail.com>
2023-11-30 11:40:57 -0500

    merging commit

--- routes/index.js
+++ routes/index.js
@@ -23,8 +23,8 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect: '/videogames',
-    failureRedirect: '/videogames'
+    successRedirect: '/',
+    failureRedirect: '/'
   }
 ));
 
